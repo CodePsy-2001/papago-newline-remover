1e30866afd4af43338bfabc56a92d4084107b4bc
CodePsy-2001 <codepsy2001@gmail.com>
2024-04-15 01:26:14 +0900

    hotfix

--- background.js
+++ background.js
@@ -1,10 +1,20 @@
 chrome.commands.onCommand.addListener((command) => {
-    if (command === "remove-newlines") {
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            chrome.scripting.executeScript({
-                target: {tabId: tabs[0].id},
-                files: ['content_script.js']
-            });
-        });
-    }
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: replace,
+    });
+  });
 });
+
+function replace() {
+  const textarea = document.querySelector("div#sourceEditArea textarea");
+
+  if (textarea) {
+    textarea.value = textarea.value
+      // remove only one newline
+      .replace(/(?<!\n)\n(?!\n)/g, " ")
+      // remove '-' for word continue
+      .replace(/(\S)-\s|\s-(\S)/g, "$1$2");
+  }
+}
